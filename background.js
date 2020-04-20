c75839e5ab108ae353db4b826fcd5a2f93c3eb9a
Drew Wagner <drewarthurwagner@gmail.com>
2020-04-19 22:52:13 -0400

    changed to prod urls

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'localhost', ports: [5000]}
+        pageUrl: { hostEquals: 'www.primevideo.com'}
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }])
